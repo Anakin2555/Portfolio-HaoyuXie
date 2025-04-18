c17ab2047d94b68bcaac55296aa9353f85cdabef
Anakin <919673126@qq.com>
2025-04-18 08:08:53 +0800

    中间件解决ssl 问题

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,5 @@
 // export const API_URL = 'http://localhost:3001/api';
+// export const API_URL = 'http://118.31.186.204:3001/api';
 
-export const API_URL = '/api/proxy';
+// 使用相对路径，这样会自动使用当前域名
+export const API_URL = '/api';
